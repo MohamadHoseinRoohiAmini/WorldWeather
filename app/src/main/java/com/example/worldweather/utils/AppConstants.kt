03fa0f56405c4b777a9e5e4afa58f942fc6090e2
MohamadHosein <84323291+MohamadHoseinRoohiAmini@users.noreply.github.com>
2025-07-17 14:43:00 +0330

    Update AppConstants.kt

--- app/src/main/java/com/example/worldweather/utils/AppConstants.kt
+++ app/src/main/java/com/example/worldweather/utils/AppConstants.kt
@@ -2,6 +2,6 @@ package com.example.worldweather.utils
 
 import com.google.android.gms.maps.model.LatLng
 
-const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+const val BASE_URL = "YOURURL"
 val TEHRAN_LAT_LNG = LatLng(35.6889744,51.4004797)
 const val REQUEST_LOCATION_PERMISSION = 1000
